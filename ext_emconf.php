cc644bd80e0f9ed57662c10a7fc609f9d89b2e74
1stmachine <1stmachine@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-06-23 13:28:40 +0000

    New Feature CSS Layout thanks to: Alexander Merzenich <amerzenich@googlemail.com>
    change: version Requirement for TER
    
    
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/csvdisplay/trunk@76690 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_emconf.php
+++ ext_emconf.php
@@ -3,45 +3,47 @@
 /***************************************************************
  * Extension Manager/Repository config file for ext "csvdisplay".
  *
- * Auto generated 09-06-2013 21:38
+ * Auto generated 23-06-2013 15:10
  *
  * Manual updates:
  * Only the data in the array - everything else is removed by next
  * writing. "version" and "dependencies" must not be touched!
  ***************************************************************/
 
-$EM_CONF[$_EXTKEY] = array (
-        'title' => 'CSV Display',
-        'description' => 'Displays CSV Data In a HTML Table, options to generate Headers from first data row and display data in multiple pages',
-        'category' => 'fe',
-        'shy' => 0,
-        'version' => '0.3.2',
-        'dependencies' => '',
-        'conflicts' => '',
-        'priority' => '',
-        'loadOrder' => '',
-        'module' => '',
-        'state' => 'beta',
-        'uploadfolder' => 1,
-        'createDirs' => '',
-        'modify_tables' => '1',
-        'clearcacheonload' => 1,
-        'lockType' => '',
-        'author' => 'Christian Wolff',
-        'author_email' => 'chris@connye.com',
-        'author_company' => '',
-        'CGLcompliance' => NULL,
-        'CGLcompliance_note' => NULL,
-        'constraints' =>
-        array (
-                'depends' =>
-                array (
-                ),
-                'conflicts' => '',
-                'suggests' =>
-                array (
-                ),
-        ),
+$EM_CONF[$_EXTKEY] = array(
+	'title' => 'CSV Display',
+	'description' => 'Displays CSV Data In a HTML Table, options to generate Headers from first data row and display data in multiple pages',
+	'category' => 'fe',
+	'shy' => 0,
+	'version' => '0.4.0',
+	'dependencies' => '',
+	'conflicts' => '',
+	'priority' => '',
+	'loadOrder' => '',
+	'module' => '',
+	'state' => 'beta',
+	'uploadfolder' => 1,
+	'createDirs' => '',
+	'modify_tables' => 1,
+	'clearcacheonload' => 1,
+	'lockType' => '',
+	'author' => 'Christian Wolff',
+	'author_email' => 'chris@connye.com',
+	'author_company' => '',
+	'CGLcompliance' => '',
+	'CGLcompliance_note' => '',
+	'constraints' => array(
+		'depends' => array(
+			'typo3' => '4.5.0-6.1.99',
+		),
+		'conflicts' => array(
+		),
+		'suggests' => array(
+		),
+	),
+	'suggests' => array(
+	),
+	'_md5_values_when_last_written' => 'a:15:{s:9:"ChangeLog";s:4:"0fa6";s:15:"CSV_Analyze.php";s:4:"dfbd";s:21:"ext_conf_template.txt";s:4:"5433";s:12:"ext_icon.gif";s:4:"7994";s:17:"ext_localconf.php";s:4:"01d5";s:14:"ext_tables.php";s:4:"4525";s:14:"ext_tables.sql";s:4:"189d";s:13:"locallang.xml";s:4:"9247";s:17:"locallang__db.xml";s:4:"89dc";s:16:"locallang_db.xml";s:4:"a547";s:10:"README.txt";s:4:"78be";s:14:"doc/manual.sxw";s:4:"4d86";s:19:"doc/wizard_form.dat";s:4:"705c";s:20:"doc/wizard_form.html";s:4:"1408";s:31:"pi1/class.tx_csvdisplay_pi1.php";s:4:"c526";}',
 );
 
 ?>
--- ext_tables.php
+++ ext_tables.php
@@ -1,99 +1,100 @@
 <?php
-if (!defined ('TYPO3_MODE'))         die ('Access denied.');
+if (!defined ('TYPO3_MODE'))	die ('Access denied.');
 
 $confArr = unserialize($GLOBALS['TYPO3_CONF_VARS']['EXT']['extConf']['csvdisplay']);
 
 $tempColumns = Array (
-        "tx_csvdisplay_colheader" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_colheader",
-                "config" => Array (
-                        "type" => "input",
-                        "size" => "30",
-                )
-        ),
-        "tx_csvdisplay_layout" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_layout",
-                "config" => Array (
-                        "type" => "input",
-                        "size" => "20",
-                )
-        ),
-        "tx_csvdisplay_firstdatarow" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow",
-                "config" => Array (
-                        "type" => "select",
-                        "items" => Array (
-                                Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.0", "0"),
-                                Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.1", "1"),
-                                Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.2", "2"),
-                        ),
-                        "size" => 1,
-                        "maxitems" => 1,
-                )
-        ),
-        "tx_csvdisplay_csvfile" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_csvfile",
-                "config" => Array (
-                        "type" => "group",
-                        "internal_type" => "file",
-                        "allowed" => "txt,csv",
-                        "disallowed" => "",
-                        "max_size" => 1000,
-                        "uploadfolder" => "uploads/tx_csvdisplay",
-                        "size" => 1,
-                        "minitems" => 0,
-                        "maxitems" => 1,
-                )
-        ),
-        "tx_csvdisplay_pageitems" => Array (
-        "exclude" => 1,
-        "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_pageitems",
-        "config" => Array (
-            "type" => "input",
-            "size" => "4",
-            "max" => "4",
-            "eval" => "int",
-            "checkbox" => "0",
-            "range" => Array (
-                "upper" => "1000",
-                "lower" => "10"
-            ),
-            "default" => 0
-        )
-    ),
-        "tx_csvdisplay_charsetconv" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_charsetconv",
-                "config" => Array (
-                        "type" => "input",
-                        "size" => "30",
-                )
-        ),
-        "tx_csvdisplay_autolink" => Array (
-        "exclude" => 1,
-        "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_autolink",
-        "config" => Array (
-            "type" => "check",
-            "default" => 1,
-        )
-    ),
-        "tx_csvdisplay_delimiter" => Array (
-                "exclude" => 1,
-                "label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_delimiter",
-                "config" => Array (
-                        "type" => "input",
-                        "size" => "1",
-                )
-        ),
+	"tx_csvdisplay_colheader" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_colheader",
+		"config" => Array (
+			"type" => "input",
+			"size" => "30",
+		)
+	),
+	"tx_csvdisplay_layout" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_layout",
+		"config" => Array (
+			"type" => "input",
+			"size" => "20",
+			"eval" => "trim,alphanum_x",
+		)
+	),
+	"tx_csvdisplay_firstdatarow" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow",
+		"config" => Array (
+			"type" => "select",
+			"items" => Array (
+				Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.0", "0"),
+				Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.1", "1"),
+				Array("LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_firstdatarow.I.2", "2"),
+			),
+			"size" => 1,
+			"maxitems" => 1,
+		)
+	),
+	"tx_csvdisplay_csvfile" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_csvfile",
+		"config" => Array (
+			"type" => "group",
+			"internal_type" => "file",
+			"allowed" => "txt,csv",
+			"disallowed" => "",
+			"max_size" => 1000,
+			"uploadfolder" => "uploads/tx_csvdisplay",
+			"size" => 1,
+			"minitems" => 0,
+			"maxitems" => 1,
+		)
+	),
+	"tx_csvdisplay_pageitems" => Array (
+	"exclude" => 1,
+	"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_pageitems",
+	"config" => Array (
+		"type" => "input",
+		"size" => "4",
+		"max" => "4",
+		"eval" => "int",
+		"checkbox" => "0",
+		"range" => Array (
+		"upper" => "1000",
+		"lower" => "10"
+		),
+		"default" => 0
+	)
+	),
+	"tx_csvdisplay_charsetconv" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_charsetconv",
+		"config" => Array (
+			"type" => "input",
+			"size" => "30",
+		)
+	),
+	"tx_csvdisplay_autolink" => Array (
+	"exclude" => 1,
+	"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_autolink",
+	"config" => Array (
+		"type" => "check",
+		"default" => 1,
+	)
+	),
+	"tx_csvdisplay_delimiter" => Array (
+		"exclude" => 1,
+		"label" => "LLL:EXT:csvdisplay/locallang_db.xml:tt_content.tx_csvdisplay_delimiter",
+		"config" => Array (
+			"type" => "input",
+			"size" => "1",
+		)
+	),
 );
 
 if ($confArr['useFileReference']) {
-        $tempColumns['tx_csvdisplay_csvfile']['config']['internal_type']='file_reference';
-        unset($tempColumns['tx_csvdisplay_csvfile']['config']['uploadfolder']);
+	$tempColumns['tx_csvdisplay_csvfile']['config']['internal_type']='file_reference';
+	unset($tempColumns['tx_csvdisplay_csvfile']['config']['uploadfolder']);
 }
 
 // tx_csvdisplay_charsetconv
